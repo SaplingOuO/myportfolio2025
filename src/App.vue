d62f0f12eb37b51220bc931ff7efa3609f630bbf
SBG_DESKTOP <gba2006su06@gmail.com>
2025-10-05 12:15:33 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import BackgroundVisual from './components/BackgroundVisual.vue';
+</script>
+
+<template>
+  <div>
+    <BackgroundVisual/>
+  </div>
+</template>
+
+<style>
+body{
+  width: 100%;
+}
+</style>
